5d381d9f0d19ac569806c39409de8047c7b1d6ec
Anais Leseur <anais.leseur@kpa6t.fr>
2022-02-11 15:11:01 +0100

    display products per page + pagination

--- src/app/components/store/store.component.ts
+++ src/app/components/store/store.component.ts
@@ -9,18 +9,31 @@ import {Product} from "../../models/product";
 })
 export class StoreComponent implements OnInit {
   public selectedCategory:any;
+  public productPerPage = 4;
+  public selectedPage = 1;
 
   constructor(private repoistory: ProductRepository) { }
 
   ngOnInit(): void {
   }
   get products (): Product[] {
-    return this.repoistory.getProducts(this.selectedCategory);
+    let pageIndex = (this.selectedPage -1)*this.productPerPage;
+    return this.repoistory.getProducts(this.selectedCategory).slice(pageIndex, pageIndex + this.productPerPage);
   }
   get categories (): (string | undefined)[] {
     return this.repoistory.getCategories()
   }
   changeCategory (newCategory?: string) {
     this.selectedCategory = newCategory;
+  }
+  changePage (newPage:number) {
+    this.selectedPage = newPage;
+  }
+  changePageSize (newSize:number) {
+    this.productPerPage = Number(newSize);
+    this.changePage(1);
+  }
+  get pageNumbers ():number[] {
+    return Array(Math.ceil(this.repoistory.getProducts(this.selectedCategory).length/this.productPerPage)).fill(0).map((x, i)=>i+1);
   }
 }
